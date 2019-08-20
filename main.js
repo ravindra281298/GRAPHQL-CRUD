fb1d31a35be5ea6ea91b7ef9795a9b2f6f1a3f8d
ravindra281298 <ravindra281298@gmail.com>
2019-08-20 17:17:46 +0530

    integrated with mongodb

--- main.js
+++ main.js
@@ -1,103 +1,74 @@
 const express=require('express');
 const ExpressGraphQL=require('express-graphql');
+const MongoClient=require('mongodb').MongoClient;
+const assert=require('assert');
 
 const{
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLNonNull,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 }=require('graphql');
 
 const app=express();
 
 
+const url='mongodb://localhost:27017';
+const dbName='confusion';
+const myColl='books';
+var bd=1
+var coll=1;
 
-const authors=[
-    {id:1,name:"J. K. Rowling"},
-    {id:2,name:"J. R. R. Tolkien"},
-    {id:3,name:"Brent Weeks"}
-]
-
-const books=[
-    {id:1,name:"Harry Potter and the Chamber of secrets",authorId:1},
-    {id:2,name:"Harry potter and the Prisoner of Azkaban",authorId:1},
-    {id:3,name:"Harry potter and the Goblet of Fire",authorId:1},
-    {id:4,name:"The Fellowship of the Ring",authorId:2},
-    {id:5,name:"The Two Towers",authorId:2},
-    {id:6,name:"The return of the King",authorId:2},
-    {id:7,name:"The way of Shadows",authorId:3},
-    {id:8,name:"Beyond the Shadows",authorId:3},
-]
-
-const authorType=new GraphQLObjectType({
-    name:'authors',
-    description:'authorType',
-    fields:()=>({
-        id:{type:GraphQLNonNull(GraphQLInt)},
-        name:{type:GraphQLString},
-        books:{
-            type:GraphQLList(bookType),
-            resolve:(author)=>(books.filter(book=>book.authorId===author.id))
-        }
-    })
+MongoClient.connect(url,(err,client)=>{
+    assert.equal(err,null);
+    db=client.db(dbName);
+    coll=db.collection(myColl);
+    console.log(`connected to database: ${dbName}`);
 });
 
 const bookType=new GraphQLObjectType({
-    name:'book',
-    description:'bookType',
+    name:'bookType',
+    description:'This is bookType',
     fields:()=>({
-        id:{type:GraphQLNonNull(GraphQLInt)},
+         _id:{type:GraphQLString},
+        id:{type:GraphQLInt},
         name:{type:GraphQLString},
-
-        author:{
-            type:GraphQLList(authorType),
-            resolve:(book)=>(authors.filter(author=>author.id===book.authorId))
-        }
+        authorId:{type:GraphQLInt}
     })
 });
-
 
 const rootQueryType=new GraphQLObjectType({
     name:'query',
-    description:'inside root query',
-    // fields:()=>({book:{},books:{},author:{}})   // structure of query
+    description:'this is root query',
     fields:()=>({
+        books:{
+            type:GraphQLList(bookType),
+            resolve:async ()=> await coll.find().toArray()
+        },
         book:{
             type:bookType,
-            description:'display single book',
             args:{
                 id:{type:GraphQLInt}
             },
-            resolve:(root,args)=>books.find(book=>book.id===args.id)
-        },
-        books:{
-            type:GraphQLList(bookType),
-            description:'display all books',
-            resolve:()=>books
-        },
-        author:{
-            type:GraphQLList(authorType),
-            description:'all authors',
-            resolve:()=>authors
+            resolve:async (root,args)=>await coll.findOne({id:args.id})
         }
     })
-});
-
-
+})
 
 const schema=new GraphQLSchema({
     query:rootQueryType
 });
 
+
 app.use('/',ExpressGraphQL({
-    schema:schema,
+   schema:schema,
     graphiql:true
 }));
 
 const hostname='localhost';
-const port=3000;
+const port=4000;
 app.listen(port,hostname,()=>{
     console.log(`listening on http://${hostname}:${port}`);
 });
