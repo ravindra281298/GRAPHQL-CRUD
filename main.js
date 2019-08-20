fdb2c1e00a33046f2d503d67c3ba73ac6986f552
ravindra281298 <ravindra281298@gmail.com>
2019-08-20 18:23:00 +0530

    integrated mongodb

--- main.js
+++ main.js
@@ -17,25 +17,44 @@ const app=express();
 
 const url='mongodb://localhost:27017';
 const dbName='confusion';
-const myColl='books';
-var bd=1
-var coll=1;
+const myColl1='books',myColl2='authors';
+var db=1,books=1,authors=1;
 
 MongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
     db=client.db(dbName);
-    coll=db.collection(myColl);
-    console.log(`connected to database: ${dbName}`);
+    books=db.collection(myColl1);
+    console.log(`connected to database: ${dbName}:${myColl1}`);
+    db=client.db(dbName);
+    authors=db.collection(myColl2);
+    console.log(`connected to database: ${dbName}:${myColl2}`);
 });
 
 const bookType=new GraphQLObjectType({
     name:'bookType',
     description:'This is bookType',
     fields:()=>({
-         _id:{type:GraphQLString},
+        _id:{type:GraphQLString},
         id:{type:GraphQLInt},
         name:{type:GraphQLString},
-        authorId:{type:GraphQLInt}
+        author:{
+            type:authorsType,
+            resolve:(book)=>authors.findOne({id:book.authorId})
+        }
+    })
+});
+
+const authorsType=new GraphQLObjectType({
+    name:'authorsType',
+    description:'This is authorType',
+    fields:()=>({
+        _id:{type:GraphQLString},
+        id:{type:GraphQLInt},
+        name:{type:GraphQLString},
+        book:{
+            type:GraphQLList(bookType),
+            resolve:(author)=> books.find({authorId:author.id}).toArray()
+        }
     })
 });
 
@@ -45,14 +64,25 @@ const rootQueryType=new GraphQLObjectType({
     fields:()=>({
         books:{
             type:GraphQLList(bookType),
-            resolve:async ()=> await coll.find().toArray()
+            resolve:async ()=> await books.find().toArray()
         },
         book:{
             type:bookType,
             args:{
                 id:{type:GraphQLInt}
             },
-            resolve:async (root,args)=>await coll.findOne({id:args.id})
+            resolve:async (root,args)=>await books.findOne({id:args.id})
+        },
+        authors:{
+            type:GraphQLList(authorsType),
+            resolve:async ()=> await authors.find().toArray()
+        },
+        author:{
+            type:authorsType,
+            args:{
+                id:{type:GraphQLInt}
+            },
+            resolve:async (root,args)=> await authors.findOne({id:args.id})
         }
     })
 })
